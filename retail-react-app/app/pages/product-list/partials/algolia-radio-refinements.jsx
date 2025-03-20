e79cddca8e50c76616b29f7d9ceb800f74c81f26
peterdj <peterhale.soundmail@gmail.com>
2025-03-20 11:05:13 -0600

    Change how the radio refinement works

--- retail-react-app/app/pages/product-list/partials/algolia-radio-refinements.jsx
+++ retail-react-app/app/pages/product-list/partials/algolia-radio-refinements.jsx
@@ -4,19 +4,39 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Box, Text, Radio, RadioGroup, Stack} from '@chakra-ui/react'
 import {useNumericMenu} from 'react-instantsearch-core'
 import PropTypes from 'prop-types'
 
 const AlgoliaRadioRefinements = (props) => {
+
+    
     const {items, refine} = useNumericMenu(props)
+
+    const findValue = () => {
+        for ( let i = 0;i < items.length;i++) {
+            if ( items[i].isRefined ) {
+                return items[i].value;
+            }
+        }
+    }
+    const val = findValue();
+    const [value, setValue] = useState(val);
+
+    if ( val !== value ) setValue(val);
+
+    const handleChange = (val) => {
+        setValue(val);
+        refine(val);
+    }
 
     return (
         <Box>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} value={value}>
                 <Stack spacing={1}>
                     {items.map((item) => {
+                        const label = `${item.label} - ${item.isRefined}`;
                         return (
                             <Box key={item.value}>
                                 <Radio
@@ -24,12 +44,10 @@ const AlgoliaRadioRefinements = (props) => {
                                     alignItems="center"
                                     height={{base: '44px', lg: '24px'}}
                                     value={item.value}
-                                    checked={item.isRefined}
-                                    onChange={() => refine(item.value)}
                                     fontSize="sm"
                                 >
                                     <Text marginLeft={-1} fontSize="sm">
-                                        {item.label}
+                                        {label}
                                     </Text>
                                 </Radio>
                             </Box>
